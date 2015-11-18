6689668bf6b89c54fd359e2e179b17534a9e116a
mbunaladi <mbunaladi@gmail.com>
2015-11-18 13:36:40 +0800

    Reuploaded Android SQLite CRUD

---
+++ crud/src/com/example/crud/DatabaseOperations.java
@@ -0,0 +1,47 @@
+package com.example.crud;
+
+import com.example.crud.TableData.TableInfo;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class DatabaseOperations extends SQLiteOpenHelper {
+	
+	public static final int database_version=1;
+	public String CREATE_QUERY="CREATE TABLE "+TableInfo.TABLE_NAME+"("
+	+TableInfo.Username+"VARCHAR,"+TableInfo.Pass+"VARCHAR);";
+
+	public DatabaseOperations(Context context) {
+		super(context, TableInfo.DATABASE_NAME, null, database_version);
+		Log.d("Database operations", "Db  created");
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase sdb) {
+		
+		sdb.execSQL(CREATE_QUERY);
+		Log.d("Databse operations", "Table created");
+
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		
+
+	}
+	
+	public void putInformation(DatabaseOperations dop, String name, String pass)
+	{
+		SQLiteDatabase SQ= dop.getWritableDatabase();
+		ContentValues cv= new ContentValues();
+		cv.put(TableInfo.Username, name);
+		cv.put(TableInfo.Pass, pass);
+		long k= SQ.insert(TableInfo.TABLE_NAME, null, cv);
+		Log.d("Database operations", "one row inserted");
+	}
+
+}
---
+++ crud/src/com/example/crud/RegisterActivity.java
@@ -0,0 +1,59 @@
+package com.example.crud;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+
+public class RegisterActivity extends Activity {
+	
+	EditText username, pass, conpass;
+	String  user_name, user_pass, con_pass;
+	Button Reg;
+	Context ctx = this;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.register_layout);
+		
+		username=(EditText) findViewById(R.id.reg_user);
+		pass = (EditText) findViewById(R.id.reg_pass);
+		conpass = (EditText) findViewById(R.id.con_pass);
+		Reg= (Button) findViewById(R.id.user_reg);
+		Reg.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				user_name= username.getText().toString();
+				user_pass= pass.getText().toString();
+				con_pass= conpass.getText().toString();
+				
+				if(!(user_pass.equals(con_pass)))
+				{
+					Toast.makeText(getBaseContext(), "Pass not match", Toast.LENGTH_LONG).show();
+					username.setText("");
+					pass.setText("");
+					conpass.setText("");
+				}
+				else
+				{
+					DatabaseOperations DB= new DatabaseOperations(ctx);
+					DB.putInformation(DB, user_name, user_pass);
+					Toast.makeText(getBaseContext(),"Registration success", Toast.LENGTH_LONG).show();
+					finish();
+				}
+			}
+		});
+		
+		
+	}
+}
