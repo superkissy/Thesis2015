97403535fac6c7f5d49d17f1effa5338e428ec7f
mbunaladi <mbunaladi@gmail.com>
2015-11-18 17:41:07 +0800

    CRUD Fix

--- test/src/com/example/test/MainActivity.java
+++ test/src/com/example/test/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -13,6 +15,16 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		Button Homepage = (Button) findViewById(R.id.tohomepage);
+		Homepage.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent Homepage_intent = new Intent(MainActivity.this, HomeActivity.class);
+				startActivity(Homepage_intent);
+			}
+		});
 	}
 
 	@Override
