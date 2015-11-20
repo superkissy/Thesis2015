5fa79b708e2ab0091fb7693b64d4f10f36e483f6
mbunaladi <mbunaladi@gmail.com>
2015-11-20 15:19:24 +0800

    SSS Main Program Login Module

--- SSS/gen/ph/gov/sss/R.java
+++ SSS/gen/ph/gov/sss/R.java
@@ -33,17 +33,25 @@ public final class R {
         public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
-        public static final int action_example=0x7f080005;
-        public static final int action_settings=0x7f080004;
-        public static final int container=0x7f080001;
-        public static final int drawer_layout=0x7f080000;
-        public static final int navigation_drawer=0x7f080002;
-        public static final int section_label=0x7f080003;
+        public static final int action_example=0x7f08000c;
+        public static final int action_settings=0x7f08000b;
+        public static final int button2=0x7f080006;
+        public static final int container=0x7f080008;
+        public static final int drawer_layout=0x7f080007;
+        public static final int imageView1=0x7f080000;
+        public static final int loginbutton=0x7f080005;
+        public static final int navigation_drawer=0x7f080009;
+        public static final int pass=0x7f080004;
+        public static final int section_label=0x7f08000a;
+        public static final int textView1=0x7f080001;
+        public static final int textView2=0x7f080003;
+        public static final int uname=0x7f080002;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
-        public static final int fragment_main=0x7f030001;
-        public static final int fragment_navigation_drawer=0x7f030002;
+        public static final int activity_login=0x7f030000;
+        public static final int activity_main=0x7f030001;
+        public static final int fragment_main=0x7f030002;
+        public static final int fragment_navigation_drawer=0x7f030003;
     }
     public static final class menu {
         public static final int global=0x7f070000;
